0c57941e44c4898820bcd87124bf2cdf2b949b66
PalermoD <palermo.deschamps@gmail.com>
2016-04-05 15:47:47 -0400

    session controller and login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_page#index'
   get 'signup' => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
