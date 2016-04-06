304ac84437f016850b34dd4cf17eea00db7a245e
PalermoD <palermo.deschamps@gmail.com>
2016-04-06 13:21:57 -0400

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+      @user = User.find(params[:id]) 
+  end
+
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_edit_params)
+        flash[:success] = "Profile updated"
+        redirect_to root_url
+      else 
+        render 'edit'
+      end 
+  end 
+
   private
 
     def user_params
