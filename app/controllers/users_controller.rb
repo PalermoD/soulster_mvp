c45e2ba213ea4b78e366d10f5e000f2f40fd2996
PalermoD <palermo.deschamps@gmail.com>
2016-04-06 13:43:33 -0400

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+before_action :logged_in_user, only: [:edit, :update]
+before_action :correct_user,   only: [:edit, :update]
+
+
   def new
   	@user = User.new 
   end
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       if @user.update_attributes(user_edit_params)
         flash[:success] = "Profile updated"
-        redirect_to root_url
+        redirect_to @user
       else 
         render 'edit'
       end 
@@ -37,5 +41,21 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def user_edit_params 
+      params.require(:user).permit(:name, :email)
+    end 
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
