f17ed424f7a4ef07953564853fdac7692dbc4689
PalermoD <palermo.deschamps@gmail.com>
2016-04-06 16:22:05 -0400

    delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,6 @@
 class PostsController < ApplicationController
-end
-
-class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
 
   def create
     @post = current_user.posts.build(post_params)
@@ -16,11 +14,19 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    flash[:success] = "post deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def post_params
       params.require(:post).permit(:content)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
