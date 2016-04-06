0905f9b29b74713a4a19114bb63895ee7a014bf8
PalermoD <palermo.deschamps@gmail.com>
2016-04-06 13:56:41 -0400

    index users show all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:edit, :update]
+before_action :logged_in_user, only: [:edit, :update, :index]
 before_action :correct_user,   only: [:edit, :update]
-
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end 
 
   def new
   	@user = User.new 
@@ -49,6 +52,7 @@ before_action :correct_user,   only: [:edit, :update]
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
