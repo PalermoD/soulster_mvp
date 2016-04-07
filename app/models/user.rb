34ec0f1088dfe2aea99bac56c3bc6a6ccadbbf0a
PalermoD <palermo.deschamps@gmail.com>
2016-04-07 12:59:01 -0400

    style changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :active_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :follower
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
