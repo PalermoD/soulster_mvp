c63be109574d18b8eb54bb189a4a9638636d6875
PalermoD <palermo.deschamps@gmail.com>
2016-04-07 11:23:37 -0400

    follow partials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+   has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :active_relationships, source: :follower
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,6 +21,22 @@ class User < ActiveRecord::Base
   def feed 
     posts
   end 
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,15 @@ root 'static_page#index'
 get 'login' => 'sessions#new'
 post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
-resources :users
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
 resources :posts, only: [:create, :destroy]
+resources :relationships,       only: [:create, :destroy]
 end
 
 
