44532a9d0c7ecaafef992a5127690989122d775e
PalermoD <palermo.deschamps@gmail.com>
2016-04-26 15:52:00 -0400

    network page headers

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,6 +2,7 @@ class StaticPageController < ApplicationController
   def index
   	if logged_in?
   	  @user = User.find_by(id: session[:user_id])
+      #@network = Network.find(params[:id])
       @post  = current_user.posts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
