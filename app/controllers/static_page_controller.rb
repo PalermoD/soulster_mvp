d8ee39238ef579a64263eaea3e17a31c20a5df6d
PalermoD <palermo.deschamps@gmail.com>
2016-04-26 13:04:27 -0400

    main path

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,13 @@
 class StaticPageController < ApplicationController
   def index
-  	 
+  	if logged_in?
+  	  @user = User.find_by(id: session[:user_id])
+      @post  = current_user.posts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+
+  def main
   	if logged_in?
   	  @user = User.find_by(id: session[:user_id])
       @post  = current_user.posts.build
