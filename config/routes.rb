2db7ad5959bc2f433703134527186b94b954ca6e
PalermoD <palermo.deschamps@gmail.com>
2016-04-26 14:12:05 -0400

    networks on home

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 get 'signup' => 'users#new'
 root 'static_page#index'
 get 'main' => 'static_page#main'
+
 get 'login' => 'sessions#new'
 post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
